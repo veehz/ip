d9dc7c34552e6f26482f8bdf9a70b9e8aa71ab36
veehz <veehzhz@gmail.com>
2025-01-17 17:10:20 +0800

    refactor: Rename project from Duke to MaidChan and update related files

---
+++ src/main/java/MaidChan.java
@@ -0,0 +1,11 @@
+public class MaidChan {
+    public static void main(String[] args) {
+        String logo = " __  __       _     _  ____ _\n"
+                    + "|  \\/  | __ _(_) __| |/ ___| |__   __ _ _ __\n"
+                    + "| |\\/| |/ _` | |/ _` | |   | '_ \\ / _` | '_ \\\n"
+                    + "| |  | | (_| | | (_| | |___| | | | (_| | | | |\n"
+                    + "|_|  |_|\\__,_|_|\\__,_|\\____|_| |_|\\__,_|_| |_|\n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
