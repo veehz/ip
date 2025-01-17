a9d3d6f87bf056fef6d67d19845456b46e2b50f7
veehz <veehzhz@gmail.com>
2025-01-17 17:37:26 +0800

    Change printing style and put message printing in sendMessage method

--- src/main/java/MaidChan.java
+++ src/main/java/MaidChan.java
@@ -1,21 +1,34 @@
+import java.util.List;
+
 public class MaidChan {
+    private static String name = "MaidChan";
     public static void main(String[] args) {
         String logo = " __  __       _     _  ____ _\n"
                     + "|  \\/  | __ _(_) __| |/ ___| |__   __ _ _ __\n"
                     + "| |\\/| |/ _` | |/ _` | |   | '_ \\ / _` | '_ \\\n"
                     + "| |  | | (_| | | (_| | |___| | | | (_| | | | |\n"
                     + "|_|  |_|\\__,_|_|\\__,_|\\____|_| |_|\\__,_|_| |_|\n";
-        String name = "MaidChan";
-
-        printHorizontalLine();
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printHorizontalLine();
+        System.out.println(logo);
+        sendMessage(List.of(
+            "Hello! I'm " + MaidChan.name + ".",
+            "What can I do for you?"
+        ));
+        sendMessage("Bye. Hope to see you again soon!");
     }
 
-    private static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
+    private static void sendMessage(String message) {
+        System.out.println("[" + MaidChan.name + "]");
+        System.out.println(message);
+        if(!message.endsWith("\n")) {
+            System.out.println();
+        }
+    }
+
+    private static <T extends Iterable<String>> void sendMessage(T messages) {
+        String allMessages = "";
+        for (String message : messages) {
+            allMessages += message + "\n";
+        }
+        sendMessage(allMessages);
     }
 }
